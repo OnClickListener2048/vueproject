bdb281f9eafc554e6420ec353130f89431c159d3
wangxin6 <wangxin6@tiens.com>
2023-03-08 13:49:47 +0800

    增加树形选择器

---
+++ src/components/TreeFilter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h4>{{ title }}</h4>
+    <el-input v-model="searchTerm"></el-input>
+    <el-tree :empty-text="emptyText" :node-key="title" default-expand-all>
+
+    </el-tree>
+  </div>
+</template>
+
+<script setup name="TreeFilter" lang="ts">
+import { ref } from "vue";
+
+
+
+interface TreeFilterProps {
+  title: string;
+  emptyText: string;
+}
+
+
+let defaults = withDefaults(defineProps<TreeFilterProps>(), {
+  title: "title",
+  emptyText: "暂无数据"
+});
+
+let searchTerm = ref<string>("");
+
+</script>
+
+<style scoped>
+
+</style>
